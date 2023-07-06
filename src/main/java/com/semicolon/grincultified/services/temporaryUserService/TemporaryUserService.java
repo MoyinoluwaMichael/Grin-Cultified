9db3e750d1a11b2668e7d2a869e5bb1ec1e2d272
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-07-06 17:31:52 +0100

    admin registration api

--- src/main/java/com/semicolon/grincultified/services/temporaryUserService/TemporaryUserService.java
+++ src/main/java/com/semicolon/grincultified/services/temporaryUserService/TemporaryUserService.java
@@ -3,7 +3,7 @@ package com.semicolon.grincultified.services.temporaryUserService;
 import com.semicolon.grincultified.dtos.requests.InvestorRegistrationRequest;
 
 public interface TemporaryUserService {
-   void addUserTemporarily(InvestorRegistrationRequest investorRegistrationRequest);
+    void addUserTemporarily(InvestorRegistrationRequest investorRegistrationRequest);
     InvestorRegistrationRequest findByEmailAddress(String emailAddress);
     void deleteTemporaryInvestor(InvestorRegistrationRequest investorRegistrationRequest);
 }
