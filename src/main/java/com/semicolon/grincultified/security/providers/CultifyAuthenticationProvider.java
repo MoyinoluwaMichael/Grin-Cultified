31ca305cd62c855a1dbc20f74a2449472899b2ec
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-07-13 12:09:38 +0100

    nothing to commit

--- src/main/java/com/semicolon/grincultified/security/providers/CultifyAuthenticationProvider.java
+++ src/main/java/com/semicolon/grincultified/security/providers/CultifyAuthenticationProvider.java
@@ -37,7 +37,6 @@ public class CultifyAuthenticationProvider implements AuthenticationProvider {
     }
 
 
-
     @Override
     public boolean supports(Class<?> authentication) {
         return authentication.equals(UsernamePasswordAuthenticationToken.class);
